b7c01a1e3713dd5a50335cc2fe7472715361372b
ahfahim5 <ahamzafahim@gmail.com>
2025-01-21 16:43:14 +0600

    Location slider Complete

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-const prevButton = document.querySelector('.prev-btn');
-const nextButton = document.querySelector('.next-btn');
-const slider = document.querySelector('.slider-cards');
-const cards = document.querySelectorAll('.card-2');
-let currentIndex = 0;
-
-function updateSliderPosition() {
-  slider.style.transition = 'transform 0.3s ease-in-out';
-  slider.style.transform = `translateX(-${currentIndex * (cards[0].offsetWidth + 10)}px)`;
-}
-
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex === 0) ? cards.length - 1 : currentIndex - 1;
-  updateSliderPosition();
-});
-
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex === cards.length - 1) ? 0 : currentIndex + 1; 
-  updateSliderPosition();
-});
-
-setInterval(() => {
-  currentIndex = (currentIndex === cards.length - 1) ? 0 : currentIndex + 1;
-  updateSliderPosition();
-}, 3000); 
-
-slider.addEventListener('transitionend', () => {
-  if (currentIndex === cards.length) {
-    slider.style.transition = 'none'; 
-    currentIndex = 0;
-    slider.style.transform = `translateX(0)`; 
-    setTimeout(() => {
-      slider.style.transition = 'transform 0.3s ease-in-out'; 
-    }, 0);
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 3,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      }
+  },
+  autoplay: {
+      delay: 3000,  
+      disableOnInteraction: false,
   }
 });
