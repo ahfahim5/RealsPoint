9a229247d05eb903cd8a1f9a5c8ac293961a045e
ahfahim5 <ahamzafahim@gmail.com>
2025-01-21 18:04:58 +0600

    FA-Slider complete

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-new Swiper('.card-wrapper', {
+new Swiper(".card-wrapper , .card-wrapper2", {
   loop: true,
   spaceBetween: 3,
   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-      0: {
-          slidesPerView: 1
-      },
-      768: {
-          slidesPerView: 2
-      },
-      1024: {
-          slidesPerView: 3
-      }
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
   autoplay: {
-      delay: 3000,  
-      disableOnInteraction: false,
-  }
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
